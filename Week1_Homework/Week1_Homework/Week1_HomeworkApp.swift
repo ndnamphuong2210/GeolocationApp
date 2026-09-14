40ce1f16177c9583c7d1695fc966720d88e31e76
Alex <ndnamphuong2210@gmail.com>
2026-09-14 17:59:14 +0700

    Add Homework

---
+++ Week1_Homework/Week1_Homework/Week1_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week1_HomeworkApp.swift
+//  Week1_Homework
+//
+//  Created by MAY 03 on 14/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Week1_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
