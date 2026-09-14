9c02c2c462775cbc606320bc38fdbbf2886d9643
Alex <ndnamphuong2210@gmail.com>
2026-09-14 14:23:14 +0700

    Update

---
+++ Week1/Week1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Week1
+//
+//  Created by MAY 03 on 14/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
