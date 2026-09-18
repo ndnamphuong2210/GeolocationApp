caa64cde70543c0e4949d246f9650d35535ca5df
Alex <ndnamphuong2210@gmail.com>
2026-09-18 16:03:28 +0700

    Add files via upload

---
+++ W1/Week1/Week1/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Week1
+//
+//  Created by MAY 03 on 14/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
